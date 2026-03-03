6c0e5901c0cf7ea691ed3b4505b55c869d96b451
unknown <rizkialiffiyyanto.nutech@gmail.com>
2026-03-03 13:45:46 +0700

    update availble rt when add rt

--- src/app/dashboard/rt/page.js
+++ src/app/dashboard/rt/page.js
@@ -269,7 +269,7 @@ export default function RtPage() {
           >
             <Checkbox.Group disabled={!selectedRwId}>
               <Space wrap>
-                {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20].map(num => (
+                {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map(num => (
                   <Checkbox key={num} value={num}>
                     RT {num}
                   </Checkbox>
